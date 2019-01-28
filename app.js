ccfcf68902c309fb49b6ae080d92a3f1d5585014
bassiebangura <devbassiebangura@gmail.com>
2019-01-28 08:04:37 -0500

    Add click event listener to start game and fxn in listener removes overlay.

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+/* gets start/reset btn and stores it in variable resetBtn */
+let resetBtn = document.getElementsByClassName("btn__reset")[0];
 
-let resetBtn = document.getElementsByClassName("btn__reset");
-console.log(resetBtn)
-resetBtn[0].addEventListener("mouserover", () => {
-    alert("hello");
+
+/* removes div with ID = overlay, and display divs with banner, phrase, keyboard and scoreboard */
+resetBtn.addEventListener("click", () => {
     document.getElementById("overlay").style.display = "none";
     }
 )
